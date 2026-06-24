eb0e3b3fe2a32fd5afce8c4586fd8e0052cb3800
Jujunju <selamatgunakan@gmail.com>
2026-06-24 19:00:38 +0700

    update mading fe axios url

--- frontend/src/data/sources/axios-instance.ts
+++ frontend/src/data/sources/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://e-mading-backend.vercel.app/api',
   withCredentials: true,
   timeout: 5000,
   // headers: {
