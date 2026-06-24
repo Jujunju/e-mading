522fee354d7b555930b80cc27fd7bdd69bb6a8cf
Jujunju <selamatgunakan@gmail.com>
2026-06-24 16:44:34 +0700

    update mading be server.ts 6

--- backend/src/infrastructure/databases/mongodb/config/db-mongodb.config.ts
+++ backend/src/infrastructure/databases/mongodb/config/db-mongodb.config.ts
@@ -3,7 +3,7 @@ import "dotenv/config"
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI!);
+        await mongoose.connect(process.env.MONGO_URI as string);
         console.log(`🍃 Database Ok`)
     } catch (error) {
         console.log(`🍃 Database Not Ok ${error}`);
